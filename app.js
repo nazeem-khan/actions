8f10488e663e21560cbea4b0b58d73c0b916d1c9
nazeem <nazeem358@gmail.com>
2020-12-30 15:53:45 +0530

    ty

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const app = express();
 var childProcess = require('child_process');
+const delay = require('delay');
 var githubUsername = 'nazeem-khan'
-let secret = 'secret';
+
 
 app.post("/webhooks/github", async function (req, res) {
-    var sender = req.body.sender;
-    var branch = req.body.ref;
-    let gitSecret = req.body.secret;
     await delay(50000)
-    if(branch.indexOf('master') > -1 && sender.login === githubUsername && gitSecret == secret){
         deploy(res);
-    }
-    else {
-        res.send(500);
-    }
 })
 
 function deploy(res){
