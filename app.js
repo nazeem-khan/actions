81f1afe5b0c666d91aea0e752d50762367e38905
nazeem <nazeem358@gmail.com>
2021-01-01 03:39:35 +0530

    commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 var {exec }= require('child_process');
 const delay = require('delay');
 var githubUsername = 'nazeem-khan'
+const axios = require("axios").default;
+
+app.use(express.json());
 
 
-app.post("/webhooks/github", async function (req, res) {
-        deploy(res);
-})
+// app.post("/webhooks/github", async function (req, res) {
+//         deploy(res);
+// })
 
 function deploy(res){
-    exec('exit');
-    exec('/home/ubuntu/deploy.sh', function(err, stdout, stderr){
+    exec('cd /home/ubuntu/pro/actions && ./dep.sh', function(err, stdout, stderr){
         if (err) {
          console.error(err);
          return res.send(500);
@@ -23,8 +26,31 @@ function deploy(res){
 
 
 app.get("/", (req, res) => {
-    res.send("loooolS");
+    res.send("gg boys");
 })
+
+app.post("/github", (req, res) => {
+  deploy(res);
+    const content = "Successfully Deployed";
+    const avatarUrl = "https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif";
+    axios
+      .post(process.env.DISCORD_WEBHOOK_URL, {
+        content: content,
+        embeds: [
+          {
+            image: {
+              url: avatarUrl,
+            },
+          },
+        ],
+      })
+      .then((discordResponse) => {
+        console.log("Success!");
+        res.status(204).send();
+      })
+      .catch((err) => console.error(`Error sending to Discord: ${err}`));
+  });
+
 
 app.listen(3000, (err) => {
     console.log('connected to 3000!!')
